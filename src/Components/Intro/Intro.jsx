a5dfe236626b357be0e2a21852e5ca57f51299ef
ganapathy <ganapathymano04@gmail.com>
2023-09-27 14:18:28 +0530

    this change was create by ganapathy

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Intro() {
+  return (
+   <div className="intro">
+    <div className="i-left">
+        <div className="i-name">
+            <span>Hy! I Am</span>
+            <span>Ganapathy Mano</span>
+            <span>Frontend Developer with high level of experience in web designing and development, producting the Quality work</span>
+        </div>
+    </div>
+    <div className="i-right"></div>
+   </div>
+  )
+}
+
+export default Intro
