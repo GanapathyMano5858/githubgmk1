11fc30dfb92dad8734ebab08797d3fbe13c87a48
ganapathy <ganapathymano04@gmail.com>
2023-09-27 17:36:55 +0530

    this changes also changed by Ganapathy ManO

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="archievement">
+        <div className="circle">3+</div>
+        <span>years</span>
+        <span>Experience</span>
+      </div>
+      <div className="archievement">
+        <div className="circle">10+</div>
+        <span>Completed</span>
+        <span>Projects</span>
+      </div>
+      <div className="archievement">
+        <div className="circle">2+</div>
+        <span>Companies</span>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Services.css";
-// import
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import Ganapathy_Mano_CV from "./Ganapathy_Mano_CV.pdf";
 
 const Services = () => {
   return (
@@ -15,11 +19,42 @@ const Services = () => {
           corrupti asperiores. Ducimus, autem qui tenetur in asperiores
           voluptatum veniam accusamus.
         </spane>
-        <button className="button s-button">Download CV</button>
-        <div className="blur s-blur1" style={{background:"ABF1FF94"}}></div>
+        <a href={Ganapathy_Mano_CV} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#C1F5FF" }}></div>
       </div>
       {/* right side */}
-      <div className="cards">I am the right side</div>
+      <div className="cards">
+        <div style={{ left: "14rem" }}>
+          {/* first card */}
+          <Card
+            emoji={HeartEmoji}
+            heading={"Design"}
+            detail={"Figma, Photoshop, Illustrator, Adobe Xd"}
+          />
+        </div>
+        {/* second card */}
+        <div style={{ top: "12rem", left: "-4rem" }}>
+          <Card
+            emoji={Glasses}
+            heading={"Developer"}
+            detail={"HTML5, CSS3, Bootstrap, JavaScript, ReactJS"}
+          />
+        </div>
+        {/* third card */}
+        <div style={{ top: "19rem", left: "12rem" }}>
+          <Card
+            emoji={Humble}
+            heading={"UI/UX"}
+            detail={"Lorem ipsum, dolor sit amet consectetur adipisicing elit."}
+          />
+        </div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
     </div>
   );
 };
