38a02c27ec28fd372dd00e53de60c43d4cce7715
ganapathy <ganapathymano04@gmail.com>
2023-09-29 19:52:12 +0530

    hi this a last update for ganapathy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Services from "./Components/Services/Services";
 import Experience from "./Components/Experience/Experience";
 import Works from "./Components/Works/Works";
 import Portfolio from "./Components/Portfolio/Portfolio";
-import Testimonial from "./Components/Testimonial/Testimonial";
+// import Testimonial from "./Components/Testimonial/Testimonial";
 import Contact from "./Components/Contact/Contact.jsx";
 import Footer from "./Components/Footer/Footer";
 import { themeContext } from "./Context";
